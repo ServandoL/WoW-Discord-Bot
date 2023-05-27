81bb110994a94ce62b04f2628debc9c87ddb4d45
servandoluviano <swyswy214@gmail.com>
2023-05-27 17:08:22 -0500

    lint

--- index.ts
+++ index.ts
@@ -9,12 +9,19 @@ DiscordClient.start({ intents: [GatewayIntentBits.Guilds] });
 DiscordClient.instance.initialize();
 
 DiscordClient.instance.events.forEach((ev) => {
-  if (ev.once) {
-    DiscordClient.instance.once(ev.name, (...args) => ev.execute(...args));
+  if (ev.once !== undefined) {
+    DiscordClient.instance.once(ev.name, async (...args) => {
+      await ev.execute(...args);
+    });
   } else {
-    DiscordClient.instance.on(ev.name, (...args) => ev.execute(...args));
+    DiscordClient.instance.on(ev.name, async (...args) => {
+      await ev.execute(...args);
+    });
   }
 });
 
 // Log in to Discord with your client's token
-DiscordClient.instance.login(AppConfig.instance.token);
+DiscordClient.instance.login(AppConfig.instance.token).catch((error) => {
+  console.error('An error occurred while the bot was trying to log in.');
+  throw new Error(error);
+});
