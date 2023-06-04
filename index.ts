7fafaa7d78196960fdb012360cd2a007f3fecd14
servandoluviano <swyswy214@gmail.com>
2023-06-04 11:47:01 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -5,6 +5,19 @@ import { BnetHttpClient } from './src/common/BnetHttpClient';
 import express from 'express';
 
 const app = express();
+app.use('/', (_req, res) => {
+  const message = {
+    message:
+      // eslint-disable-next-line quotes
+      "Thank you for using my bot. I don't collect any of your information but I do use Blizzard's services for my data. As per Blizzard, I have to include their Privacy Policy. You can view it here: https://www.blizzard.com/en-us/legal/a4380ee5-5c8d-4e3b-83b7-ea26d01a9918/blizzard-entertainment-online-privacy-policy"
+  };
+  try {
+    res.status(200).send(message);
+  } catch (error) {
+    message.message = JSON.stringify(error);
+    res.status(503).send(message);
+  }
+});
 app.use('/health', (_req, res) => {
   const health = {
     uptime: process.uptime(),
