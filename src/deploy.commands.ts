b66f8e04e29cdc8f0259c43070b6220af14b9152
servandoluviano <swyswy214@gmail.com>
2023-06-04 15:23:24 -0500

    Update deploy.commands.ts

--- src/deploy.commands.ts
+++ src/deploy.commands.ts
@@ -6,16 +6,12 @@ export async function deployCommands(): Promise<void> {
   const commands: RESTPostAPIChatInputApplicationCommandsJSONBody[] = DiscordClient.instance
     .getAllCommands()
     .map((command) => command.data.toJSON());
-
-  const rest = new REST().setToken(AppConfig.instance.token);
   try {
+    const rest = new REST().setToken(AppConfig.instance.token);
     console.log(`Started refreshing ${commands.length} application (/) commands.`);
-    const data = await rest.put(
-      Routes.applicationGuildCommands(AppConfig.instance.clientId, AppConfig.instance.guildId),
-      {
-        body: commands
-      }
-    );
+    const data = await rest.put(Routes.applicationCommands(AppConfig.instance.clientId), {
+      body: commands
+    });
     console.log(`Successfully reloaded ${(data as any[]).length} application (/) commands.`);
   } catch (error) {
     const errorMsg = `An error occurred: ${JSON.stringify(error)}`;
