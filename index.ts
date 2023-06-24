0d3ecd581edac944caf84003625fbeaa87a1a34a
servandoluviano <swyswy214@gmail.com>
2023-06-24 15:22:05 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -4,20 +4,6 @@ import { AppConfig } from './src/AppConfig';
 import { logger } from './src/logger';
 (async () => {
   const app = express();
-  app.get('/', (_req, res) => {
-    const message = {
-      message:
-        // eslint-disable-next-line quotes
-        "Thank you for using my bot. I don't collect any of your information but I do use Blizzard's services for my data. As per Blizzard, I have to include their Privacy Policy. You can view it here: https://www.blizzard.com/en-us/legal/a4380ee5-5c8d-4e3b-83b7-ea26d01a9918/blizzard-entertainment-online-privacy-policy"
-    };
-    try {
-      logger.info(`Request headers: ${JSON.stringify(_req.headers)}`);
-      res.status(200).send(message);
-    } catch (error) {
-      message.message = JSON.stringify(error);
-      res.status(503).send(message);
-    }
-  });
   app.get('/health', (_req, res) => {
     const health = {
       uptime: process.uptime(),
